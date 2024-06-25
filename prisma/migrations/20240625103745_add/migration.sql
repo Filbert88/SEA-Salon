e83d8f4b257dbd9798b5231a9aceb377fb569acd
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-06-25 22:29:54 +0700

    feat: update schema

---
+++ prisma/migrations/20240625103745_add/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `description` to the `Service` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `price` to the `Service` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "description" TEXT NOT NULL,
+ADD COLUMN     "price" BIGINT NOT NULL;
